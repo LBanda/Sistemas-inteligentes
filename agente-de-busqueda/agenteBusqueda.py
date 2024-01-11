d773e70847aa075c765ec184b49b1497306ae640
LBanda <A01610329@itesm.mx>
2024-01-10 19:14:06 -0600

    Update agenteBusqueda.py

--- agente-de-busqueda/agenteBusqueda.py
+++ agente-de-busqueda/agenteBusqueda.py
@@ -7,7 +7,8 @@ class Node:
 
     def __str__(self):
         return f"State: {self.state} Action: {self.action}\n"
-    
+
+
 class Environment:
     def __init__(self, maze):
         self.walls = maze[0]
@@ -25,73 +26,75 @@ class SearchAgent:
         self.goal_state = environment.goal
 
     def print_frontier(self):
-        print("Frontier:")
-        print("----------")
+        print('Frontier:')
+        print('----------')
         for node in self.frontier:
             print(node)
-
+    
     def print_explored(self):
-        print("Explored:")
-        print("----------")
+        print('Explored :')
+        print('----------')
         for node in self.explored:
             print(node)
 
+
     def is_frontier_empty(self):
         return len(self.frontier) == 0
+
+    def remove_node(self):
+        pass
 
     def is_goal(self, node):
         if node.state == self.goal_state:
             self.solution = []
             while node.parent is not None:
-                self.solution.append(node)
-                print(f"{node.state}({node.action})")
-                node = node.parent
-            raise Exception("Solution Found!!!")
-
+               self.solution.append(node)
+               print(f"{node.state}({node.action})")
+               node = node.parent
+            
+            raise Exception("Solution found !!!")
+    
     def add_to_frontier(self, node):
-        self.frontier.add(node)
+        self.frontier.append(node)
+
 
     def add_to_explored(self, node):
         self.explored.add(node)
 
-    def is_state_in_frontier(self,node):
-        return any(frontier_node.state == node.state for frontier_node in self.frontier)
+    def is_state_in_frontier(self, node):
+        return any(frontier_node.state==node.state for frontier_node in self.frontier)
     
-    def is_state_in_explored(self,node):
-        return any(explored_node.state == node.state for explored_node in self.explored)
-
-    def actions(self, node):
-        pass
-
-    def remove_node(self):
-        pass
-
-    def result(self, node):
-        pass
+    def is_state_in_explored(self, node):
+        return any(explored_node.state==node.state for explored_node in self.explored)
 
     def expand_node(self, node):
         for child in self.result(node):
             if not self.is_state_in_frontier(child) and not self.is_state_in_explored(child):
                 self.add_to_frontier(child)
-    
+
     def solve(self):
         while not self.is_frontier_empty():
             try:
                 node = self.remove_node()
             except:
-                print("Solution not Found!!!")
+                print("Solution not found!")
+
             try:
                 self.is_goal(node)
             except:
                 return self.solution
+
             self.add_to_explored(node)
             self.expand_node(node)
 
 
+#n = Node([1, 2, 3, 4])
+#print(n)
+
 class AgentDepthFirst(SearchAgent):
     def remove_node(self):
         return self.frontier.pop()
-    
+
 class MazeSearch(AgentDepthFirst):
     def __init__(self, environment):
         super().__init__(environment)
@@ -100,44 +103,48 @@ class MazeSearch(AgentDepthFirst):
     def actions(self, node):
         row, col = node.state
         possible_actions = [
-            ("up", (row-1, col)),
-            ("down", (row+1,col)),
-            ("left", (row, col-1)),
-            ("right", (row, col+1))
-        ]
+                ("up", (row-1, col)),
+                ("down", (row+1, col)),
+                ("left", (row, col-1)),
+                ("right", (row, col+1))
+                ]
         return possible_actions
-    
+
     def result(self, node):
         children = []
         height = len(self.walls)
         width = len(self.walls[0])
 
         for action, (r,c) in self.actions(node):
-            if 0 <= r < height and 0 <= c < width and not self.walls[r][c]:
-                children.append(Node(state=(r,c),parent=node,action=action))
-
+            if 0 <= r < height and 0 <= width and not self.walls[r][c]:
+                children.append(Node(state=(r,c), parent=node, action=action))
         return children
-    
+
 class MazeSearchBFS(MazeSearch):
     def remove_node(self):
         return self.frontier.pop(0)
 
 
 maze = ([
-    [1,1,0,0,0,0,1],
-    [1,1,0,0,0,0,1],
-    [1,1,0,0,0,0,1],
-    [1,1,0,0,0,0,1],
-    [1,1,0,0,0,0,1],
-    [1,1,0,0,0,0,1]],
-    (5,0),
-    (1,2))
+[1, 1, 0, 1, 0, 0, 1],   # walls
+[1, 1, 0, 1, 1, 0, 1],
+[1, 0, 0, 1, 0, 0, 1],
+[1, 0, 1, 1, 0, 1, 1],
+[0, 0, 0, 0, 0, 1, 1],
+[0, 1, 1, 1, 1, 1, 1]],
+(0,5), # start state
+(1,2)) # goal state
+
 
 maze_environment = Environment(maze)
-
 maze_environment.print()
 
-maze_solve = MazeSearchBFS(maze_environment)
-maze_solve.print_frontier()
-solution_nodes = maze_solve.solve()
-maze_environment.print()
+maze_solver = MazeSearch(maze_environment)
+maze_solver.print_frontier()
+
+solution_nodes = maze_solver.solve()
+
+
+maze_solver_bfs = MazeSearchBFS(maze_environment)
+maze_solver_bfs.print_frontier()
+solution_nodes = maze_solver_bfs.solve()
